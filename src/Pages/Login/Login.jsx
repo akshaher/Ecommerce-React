4060db63751333b5fda13efd7f16103f45a9b01a
Akshay Aher <akshay.aher@netcorecloud.com>
2026-05-25 10:42:58 +0530

    remove unwanted logs.

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,7 +4,7 @@ import "./login.css";
 import { fetchUserCart } from "../../store/cartStore";
 import { GoogleLogin } from '@react-oauth/google';
 
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "http://localhost:5000/";
 
 function Login() {
   const navigate = useNavigate();
@@ -19,7 +19,6 @@ function Login() {
       });
 
       const data = await response.json();    
-      console.log(data);
         
 
       if (!response.ok) {
